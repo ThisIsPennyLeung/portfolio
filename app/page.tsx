d76926f84ffbb952c593b9376b723bfd4a2ca12c
Penny Leung <thisIsPennyLeung@gmail.com>
2026-06-15 12:04:17 +0000

    Coming Soon message

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/app/components/widget/card/card"
+import { Center } from "@/app/components/widget/center/center"
+
+export const Page = async () => {
+  return (
+    <Center>
+      <Card>Status: WIP 🚧 (Target completion: Before June 22nd)</Card>
+    </Center>
+  )
+}
+export default Page
