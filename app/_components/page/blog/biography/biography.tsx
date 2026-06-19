f23881e88d8086373cf44567f236c84d47e70554
Penny Leung <thisIsPennyLeung@gmail.com>
2026-06-19 19:48:52 +0000

    biography

--- app/_components/page/blog/biography/biography.tsx
+++ app/_components/page/blog/biography/biography.tsx
@@ -1,6 +1,39 @@
 import { Card } from "@/app/_components/widget/card/card"
+import { Link } from "@/app/_components/widget/link/link"
+import { Padding } from "@/app/_components/widget/padding/padding"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faGlobe } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import styles from "./biography.module.css"
 
+const Github = () => (
+  <Link href="https://github.com/ThisIsPennyLeung/portfolio" target="blank">
+    <FontAwesomeIcon icon={faGithub} /> Github
+  </Link>
+)
+
+const Site = () => (
+  <Link href="https://ThisIsPennyLeung.dev">
+    <FontAwesomeIcon icon={faGlobe} /> This Blog
+  </Link>
+)
+
 export const Biography = () => {
-  return <Card cssClasses={styles.root}>Biography</Card>
+  const items = [
+    { key: "name", content: <h3>Penny Leung</h3> },
+    { key: "tagline", content: <span>From Legacy To Modern</span> },
+    { key: "empty", content: <></> },
+    { key: "github", content: <Github /> },
+    { key: "site", content: <Site /> },
+  ]
+
+  return (
+    <Card cssClasses={styles.root}>
+      {items.map((item) => (
+        <Padding key={item.key} direction="vertical">
+          {item.content}
+        </Padding>
+      ))}
+    </Card>
+  )
 }
