2b128c4c5910537c7e6a921d676ea4a49923c35b
Penny Leung <thisIsPennyLeung@gmail.com>
2026-07-27 13:54:36 +0000

    fix some bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { BlogListPage } from "@/app/_components/page/blog/BlogListPage"
+import { getAllBlogPosts } from "./_lib/fetch/blogPost"
 
-export const Page = async () => {
-  return <BlogListPage />
+const Page = async () => {
+  const blogPosts = await getAllBlogPosts()
+  return <BlogListPage blogPosts={blogPosts} />
 }
 export default Page
